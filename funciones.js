cde162362a5440b36c7ad14653121b87c22cdd8e
Snyder <edward.snyder219.ema4@gmail.com>
2022-10-26 19:14:36 -0500

    Video9 curso GAS avanzado

--- funciones.js
+++ funciones.js
@@ -5,3 +5,9 @@ function doGet() {
 function obtenerDatosHTML(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+    let hoja = SpreadsheetApp.openById('17r_KychvQCz1saw_wh-czk-hgHNYZV3DDV_pP2uF624').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;    
+}
